c13e169a4dd857599c78494de6202fbb15d99bff
Özlem Karakoç <ozlmkrkoc@gmail.com>
2024-03-21 13:40:42 +0300

    Repository

---
+++ DataAccessLayerr/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,47 @@
+﻿using DataAccessLayerr.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayerr.Concrete.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context c = new Context();
+        DbSet<Category> _object; 
+
+        public void Delete(Category p)
+        {
+            _object.Remove(p);
+            c.SaveChanges();
+        }
+
+        public void Insert(Category p)
+        {
+            _object.Add(p);
+            c.SaveChanges();
+        }
+
+        public List<Category> List()
+        {
+            return _object.ToList();
+        }
+
+        public void Update(Category p)
+        {
+            c.SaveChanges();
+        }
+    }
+}
+
+
+/*
+To List
+Add
+Remove
+Find
+ */
