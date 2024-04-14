f619d391dde235697ab0b793ac7b9d23ac4a331d
Özlem Karakoç <ozlmkrkoc@gmail.com>
2024-04-14 18:27:56 +0300

    About Page

--- DataAccessLayerr/EntityFramework/EfAboutDal.cs
+++ DataAccessLayerr/EntityFramework/EfAboutDal.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DataAccessLayerr.Abstract;
+using DataAccessLayerr.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayerr.EntityFramework
 {
-    public class EfAboutDal
+    public class EfAboutDal : GenericRepository<About>, IAboutDal
     {
     }
 }
